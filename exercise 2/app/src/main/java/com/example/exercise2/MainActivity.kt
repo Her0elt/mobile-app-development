338783972608c4d6fe6bbe73140ae5a1fd0261d0
her0elt <hermann.owren.elton@hotmail.com>
2021-08-30 22:31:16 +0200

    2

---
+++ exercise 2/app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.exercise2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+
+}
